e5fdad32c3ba9dd77c7423896a5459f3ed4b6b8c
vaishnavi <k12vaishnavi@gmail.com>
2022-08-17 20:18:08 +0530

    first commit

---
+++ lib/addnote.dart
@@ -0,0 +1,143 @@
+import 'dart:math';
+
+import 'package:crud/home.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class addnote extends StatefulWidget {
+  @override
+  _addnoteState createState() => _addnoteState();
+}
+
+class _addnoteState extends State<addnote> {
+  late String imageUrl;
+  TextEditingController second = TextEditingController();
+
+  TextEditingController third = TextEditingController();
+
+  final fb = FirebaseDatabase.instance;
+  @override
+  Widget build(BuildContext context) {
+    var rng = Random();
+    var k = rng.nextInt(10000);
+
+    final ref = fb.ref().child('todos/$k');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Todos"),
+        backgroundColor: Colors.purple,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+                decoration: BoxDecoration(border: Border.all()),
+                child: Column(
+                  children: <Widget>[
+                    (imageUrl != null)
+                        ? Image.network(imageUrl)
+                        : Placeholder(
+                            fallbackHeight: 200.0,
+                            fallbackWidth: double.infinity),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RaisedButton(
+                      child: Text('Upload Image'),
+                      color: Colors.lightBlue,
+                      onPressed: () => uploadImage(),
+                    )
+                  ],
+                )),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: second,
+                decoration: InputDecoration(
+                  hintText: 'title',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: third,
+                decoration: InputDecoration(
+                  hintText: 'sub title',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            MaterialButton(
+              color: Colors.purple,
+              onPressed: () {
+                ref.set({
+                  "title": second.text,
+                  "subtitle": third.text,
+                }).asStream();
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => Home()));
+              },
+              child: Text(
+                "save",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  uploadImage() async {
+    final _storage = FirebaseStorage.instance;
+    final _picker = ImagePicker();
+    PickedFile? image;
+
+    await Permission.photos.request();
+
+    var permissionStatus = await Permission.photos.status;
+
+    if (permissionStatus.isGranted) {
+      image = await _picker.getImage(source: ImageSource.gallery);
+      var file = File(image!.path);
+
+      if (image != null) {
+        var snapshot =
+            await _storage.ref().child('folderName/imageName').putFile(file);
+
+        var downloadUrl = await snapshot.ref.getDownloadURL();
+
+        setState(() {
+          imageUrl = downloadUrl;
+        });
+      } else {
+        print('No Path Received');
+      }
+    } else {
+      print('Grant Permissions and try again');
+    }
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,189 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
+
+import 'addnote.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final fb = FirebaseDatabase.instance;
+  TextEditingController second = TextEditingController();
+
+  TextEditingController third = TextEditingController();
+  var l;
+  var g;
+  var k;
+  @override
+  Widget build(BuildContext context) {
+    final ref = fb.ref().child('todos');
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        onPressed: () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (_) => addnote(),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(
+          'Todos',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        backgroundColor: Colors.purple,
+      ),
+      body: FirebaseAnimatedList(
+        query: ref,
+        shrinkWrap: true,
+        itemBuilder: (context, snapshot, animation, index) {
+          var v = snapshot.value.toString();
+
+          g = v.replaceAll(RegExp("{|}|subtitle: |title: "), "");
+          g.trim();
+
+          l = g.split(',');
+          return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            k = snapshot.key;
+                          });
+                          child:
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.edit,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ],
+                          );
+                          showDialog(
+                            context: context,
+                            builder: (ctx) => AlertDialog(
+                              title: Container(
+                                decoration: BoxDecoration(border: Border.all()),
+                                child: TextField(
+                                  controller: second,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    hintText: 'title',
+                                  ),
+                                ),
+                              ),
+                              content: Container(
+                                decoration: BoxDecoration(border: Border.all()),
+                                child: TextField(
+                                  controller: third,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    hintText: 'sub title',
+                                  ),
+                                ),
+                              ),
+                              actions: <Widget>[
+                                MaterialButton(
+                                  onPressed: () {
+                                    Navigator.of(ctx).pop();
+                                  },
+                                  color: Color.fromARGB(255, 135, 0, 145),
+                                  child: Text(
+                                    "Cancel",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                MaterialButton(
+                                  onPressed: () async {
+                                    await upd();
+                                    Navigator.of(ctx).pop();
+                                  },
+                                  color: Color.fromARGB(255, 135, 0, 145),
+                                  child: Text(
+                                    "Update",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        child: Container(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ListTile(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  color: Colors.white,
+                                ),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              tileColor: Colors.purple,
+                              trailing: IconButton(
+                                icon: Icon(
+                                  Icons.delete,
+                                  color: Color.fromARGB(255, 255, 0, 0),
+                                ),
+                                onPressed: () {
+                                  ref.child(snapshot.key!).remove();
+                                },
+                              ),
+                              title: Text(
+                                l[1],
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                l[0],
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ])
+              ]);
+        },
+      ),
+    );
+  }
+
+  upd() async {
+    DatabaseReference ref1 = FirebaseDatabase.instance.ref("todos/$k");
+
+    await ref1.update({
+      "title": second.text,
+      "subtitle": third.text,
+    });
+    second.clear();
+    third.clear();
+  }
+}
